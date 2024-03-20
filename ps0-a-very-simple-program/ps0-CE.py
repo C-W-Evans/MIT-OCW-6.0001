82456e2ac944aecb8627e2ba6e44e72cb350549e
C-W-Evans <164054958+C-W-Evans@users.noreply.github.com>
2024-03-20 12:37:19 +0100

    Add files via upload

---
+++ ps0-a-very-simple-program/ps0-CE.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+x = int(input("Please enter an interger x: "))
+y = int(input("Please enter another interger y: "))
+
+print("\nx to the power of y =",x**y)
+print("and log base 2 of x =", np.log2(x))
